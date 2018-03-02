bb0da9628e9878271a3857cff38b5859cff7dc40
Alan Zhao <alanzhaonys@yahoo.com>
2018-03-02 02:13:32 -0500

    clean up

--- docker.sh
+++ docker.sh
@@ -6,28 +6,31 @@ if [ "${1}" != "build" ] && [ "${1}" != "run" ] && [ "${1}" != "stop" ] && [ "${
   exit
 fi
 
+readonly DOCKER_TAG=wp
 readonly DOCKER_NAME=mywordpress
-CONTAINER=$(docker ps -qf name="$DOCKER_NAME")
+readonly DOCKER_PORT=5000
 
-# Build
 if [ "${1}" == "build" ]; then
 
-    docker build -t $DOCKER_NAME .
+    docker build --no-cache -t $DOCKER_TAG .
 
 elif [ "${1}" == "run" ]; then
 
-    docker run -d $DOCKER_NAME
+    docker run -d -it -p 80:$DOCKER_PORT --name $DOCKER_NAME $DOCKER_TAG
 
 elif [ "${1}" == "stop" ]; then
 
+    CONTAINER=$(docker ps -qf name="$DOCKER_NAME")
     docker stop $CONTAINER
 
 elif [ "${1}" == "remove" ]; then
 
+    CONTAINER=$(docker ps -qf name="$DOCKER_NAME")
     docker rm $CONTAINER
 
 elif [ "${1}" == "ssh" ]; then
 
+    CONTAINER=$(docker ps -qf name="$DOCKER_NAME")
     docker exec -it $CONTAINER /bin/bash
 
 fi
