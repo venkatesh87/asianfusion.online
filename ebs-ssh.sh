83e29d00ed8b76ea1fa492eb768b1ca27d513380
Alan Zhao <alanzhaonys@gmail.com>
2018-03-08 11:36:35 -0500

    ebs ssh script

---
+++ ebs-ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+readonly KEY_PATH=$(jq -r ".ec2KeyPath" ./app.json)
+readonly APP_NAME=$(jq -r ".appName" ./app.json)
+readonly BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+readonly ENV_NAME=${APP_NAME}-${BRANCH}
+
+readonly PUBLIC_IP=($(aws ec2 describe-instances --filters "Name=tag:Name,Values=mywordpress-dev" | jq -r ".Reservations[].Instances[].PublicIpAddress"))
+
+echo "Connecting to $PUBLIC_IP"
+
+ssh -i $KEY_PATH ec2-user@$PUBLIC_IP
--- load-db.sh
+++ load-db.sh
@@ -5,12 +5,12 @@ if [[ $# -eq 0 ]] ; then
   exit
 fi
 
-ENV=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-SQL=$1
+readonly ENV=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+readonly SQL_FILE=$1
 
 readonly HOST=$(jq -r ".${ENV}.endpoint" ./db.json)
 readonly DATABASE=$(jq -r ".${ENV}.database" ./db.json)
 readonly USER=$(jq -r ".${ENV}.user" ./db.json)
 readonly PASSWORD=$(jq -r ".${ENV}.password" ./db.json)
 
-mysql -h$HOST -u$USER -p$PASSWORD $DATABASE < "$SQL"
+mysql -h$HOST -u$USER -p$PASSWORD $DATABASE < "$SQL_FILE"
