365e0aeb13a85ac8605602e134e3427fb2735e89
Alan Zhao <alanzhaonys@gmail.com>
2018-03-29 03:20:27 -0400

    fix db push script

--- push-db-from-dev-to-qa.sh
+++ push-db-from-dev-to-qa.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 readonly APP_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+readonly APP_NAME=$(jq -r ".appName" ./app.json)
 
 if [ "$APP_BRANCH" != "dev" ]; then
   echo "You're not on dev branch"
   exit
 fi
 
-readonly DEV_HOST=$(jq -r ".${APP_BRANCH}.endpoint" ./db.json)
-readonly DEV_DATABASE=$(jq -r ".${APP_BRANCH}.database" ./db.json)
-readonly DEV_USER=$(jq -r ".${APP_BRANCH}.user" ./db.json)
-readonly DEV_PASSWORD=$(jq -r ".${APP_BRANCH}.password" ./db.json)
+readonly DEV_HOST=$(jq -r ".dev.endpoint" ./db.json)
+readonly DEV_DATABASE=$(jq -r ".dev.database" ./db.json)
+readonly DEV_USER=$(jq -r ".dev.user" ./db.json)
+readonly DEV_PASSWORD=$(jq -r ".dev.password" ./db.json)
 
-readonly QA_HOST=$(jq -r ".${APP_BRANCH}.endpoint" ./db.json)
-readonly QA_DATABASE=$(jq -r ".${APP_BRANCH}.database" ./db.json)
-readonly QA_USER=$(jq -r ".${APP_BRANCH}.user" ./db.json)
-readonly QA_PASSWORD=$(jq -r ".${APP_BRANCH}.password" ./db.json)
+readonly QA_HOST=$(jq -r ".qa.endpoint" ./db.json)
+readonly QA_DATABASE=$(jq -r ".qa.database" ./db.json)
+readonly QA_USER=$(jq -r ".qa.user" ./db.json)
+readonly QA_PASSWORD=$(jq -r ".qa.password" ./db.json)
 
-readonly SQL_FILE=/tmp/${DATABASE}-${APP_BRANCH}.sql
+readonly SQL_FILE=/tmp/${APP_NAME}-dev.sql
 
 mysqldump -h$DEV_HOST -u$DEV_USER -p$DEV_PASSWORD $DEV_DATABASE > "$SQL_FILE"
 
--- push-db-from-qa-to-live.sh
+++ push-db-from-qa-to-live.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 readonly APP_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+readonly APP_NAME=$(jq -r ".appName" ./app.json)
 
 if [ "$APP_BRANCH" != "qa" ]; then
   echo "You're not on qa branch"
   exit
 fi
 
-readonly QA_HOST=$(jq -r ".${APP_BRANCH}.endpoint" ./db.json)
-readonly QA_DATABASE=$(jq -r ".${APP_BRANCH}.database" ./db.json)
-readonly QA_USER=$(jq -r ".${APP_BRANCH}.user" ./db.json)
-readonly QA_PASSWORD=$(jq -r ".${APP_BRANCH}.password" ./db.json)
+readonly QA_HOST=$(jq -r ".qa.endpoint" ./db.json)
+readonly QA_DATABASE=$(jq -r ".qa.database" ./db.json)
+readonly QA_USER=$(jq -r ".qa.user" ./db.json)
+readonly QA_PASSWORD=$(jq -r ".qa.password" ./db.json)
 
-readonly LIVE_HOST=$(jq -r ".${APP_BRANCH}.endpoint" ./db.json)
-readonly LIVE_DATABASE=$(jq -r ".${APP_BRANCH}.database" ./db.json)
-readonly LIVE_USER=$(jq -r ".${APP_BRANCH}.user" ./db.json)
-readonly LIVE_PASSWORD=$(jq -r ".${APP_BRANCH}.password" ./db.json)
+readonly LIVE_HOST=$(jq -r ".master.endpoint" ./db.json)
+readonly LIVE_DATABASE=$(jq -r ".master.database" ./db.json)
+readonly LIVE_USER=$(jq -r ".master.user" ./db.json)
+readonly LIVE_PASSWORD=$(jq -r ".master.password" ./db.json)
 
-readonly SQL_FILE=/tmp/${DATABASE}-${APP_BRANCH}.sql
+readonly SQL_FILE=/tmp/${APP_NAME}-qa.sql
 
 mysqldump -h$QA_HOST -u$QA_USER -p$QA_PASSWORD $QA_DATABASE > "$SQL_FILE"
 
