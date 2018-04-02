471fabadf815a3a884f86022bf10b2a1eabe81e0
Alan Zhao <alanzhaonys@gmail.com>
2018-04-02 09:52:14 -0400

    fix bug and more plugins

--- reset-wordpress.sh
+++ reset-wordpress.sh
@@ -91,7 +91,7 @@ fi
 
 # Update Akismet plugin settings
 
-if [ "$WP_APK_KEY" != "" ]; then
+if [ "$WP_API_KEY" != "" ]; then
   readonly HAS_WP_API_KEY=$(mysql -h$DB_HOST -u$DB_USER -p$DB_PASSWORD -se "SELECT COUNT(option_id) FROM ${DB_DATABASE}.wp_options WHERE option_name = 'wordpress_api_key';")
 
   if [ "$HAS_WP_API_KEY" == 1 ]; then
@@ -148,6 +148,7 @@ fi
 # SELECT * FROM wp_options WHERE option_name = 'active_plugins';
 
 if [ "$ACTIVATE_PREINSTALLED_PLUGINS" == 1 ]; then
-  readonly WP_ACTIVATE_PLUGIN_VALUES="a:8:{i:0;s:19:\"akismet/akismet.php\";i:1;s:41:\"amazon-s3-and-cloudfront/wordpress-s3.php\";i:2;s:60:\"cf7-conditional-fields/contact-form-7-conditional-fields.php\";i:3;s:36:\"contact-form-7/wp-contact-form-7.php\";i:4;s:21:\"flamingo/flamingo.php\";i:5;s:31:\"wp-email-smtp/wp_email_smtp.php\";i:6;s:33:\"wpcf7-redirect/wpcf7-redirect.php\";i:7;s:33:\"wps-hide-login/wps-hide-login.php\";}"
+  readonly WP_ACTIVATE_PLUGIN_VALUES="a:12:{i:0;s:19:\"akismet/akismet.php\";i:1;s:41:\"amazon-s3-and-cloudfront/wordpress-s3.php\";i:2;s:60:\"cf7-conditional-fields/contact-form-7-conditional-fields.php\";i:3;s:36:\"contact-form-7/wp-contact-form-7.php\";i:4;s:23:\"elementor/elementor.php\";i:5;s:21:\"flamingo/flamingo.php\";i:6;s:51:\"header-footer-elementor/header-footer-elementor.php\";i:7;s:21:\"megamenu/megamenu.php\";i:8;s:24:\"wordpress-seo/wp-seo.php\";i:9;s:31:\"wp-email-smtp/wp_email_smtp.php\";i:10;s:33:\"wpcf7-redirect/wpcf7-redirect.php\";i:11;s:33:\"wps-hide-login/wps-hide-login.php\";}"
+  
   mysql -h$DB_HOST -u$DB_USER -p$DB_PASSWORD -e "UPDATE ${DB_DATABASE}.wp_options SET option_value = '${WP_ACTIVATE_PLUGIN_VALUES}' WHERE option_name = 'active_plugins';"
 fi
