f31368b808ce6bd4897d2f1cf37f609c290caf92
Alan Zhao <alanzhaonys@gmail.com>
2018-04-03 03:14:47 -0400

    custom login icon

--- wordpress/wp-content/themes/astra/functions.php
+++ wordpress/wp-content/themes/astra/functions.php
@@ -127,3 +127,24 @@ if ( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-filters.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-hooks.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-functions.php';
+
+
+
+/**
+ * Custom actions
+ */
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/login-logo.jpeg);
+		        height: 80px;
+		        width: 80px;
+		        background-size: 80px 80px;
+		        background-repeat: no-repeat;
+        	  padding-bottom: 30px;
+        }
+    </style>
+<?php }
+
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
