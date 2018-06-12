2d403158b3e94741effe4a7a1da79602314d7003
Alan Zhao <alanzhaonys@gmail.com>
2018-06-12 02:45:02 -0400

    fix sync creds up

--- sync-creds-up.sh
+++ sync-creds-up.sh
@@ -7,6 +7,6 @@ readonly CREDS_S3_BUCKET=$(jq -r ".aws.credsS3Bucket" $APP_CONFIG_FILE)
 aws s3 cp --profile $AWS_PROFILE ./app.json s3://$CREDS_S3_BUCKET/$APP_NAME/app.json
 aws s3 cp --profile $AWS_PROFILE ./db.json s3://$CREDS_S3_BUCKET/$APP_NAME/db.json
 
-aws s3 cp --profile $AWS_PROFILE ./bistrosol-*.json s3://$CREDS_S3_BUCKET/$APP_NAME/
+aws s3 cp --profile $AWS_PROFILE ./bistrosolutions-*.json s3://$CREDS_S3_BUCKET/$APP_NAME/
 
 echo "Successfully uploaded app.json and db.json"
