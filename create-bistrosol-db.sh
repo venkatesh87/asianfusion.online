e3e5b3d9c81546130495f5e5a2886447dab7740d
Alan Zhao <alanzhaonys@gmail.com>
2018-06-12 02:21:17 -0400

    sync db up

--- create-bistrosol-db.sh
+++ create-bistrosol-db.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#
+# Need to enable functions, procedures and triggers in RDS
+# https://aws.amazon.com/premiumsupport/knowledge-center/rds-mysql-functions/
+#
+# This script does following:
+# 1. Create database `bistrosolutions_{branch}`
+# 2. Create user `bistrosolutions_replication_{branch}`
+# 3. Create user `bistrosolutions_web_{branch}`
+# 4. Download latest database SQL from S3
+# 5. Search replace on SQL using `sed` (replace database name and remove DEFINER)
+# 6. Load database
+# 7. Export `bistrosolutions-{branch}.json`
+
 source ./functions.sh
 
 APP_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
@@ -11,11 +24,19 @@ readonly AWS_PROFILE=$(jq -r ".aws.${APP_BRANCH}.profile" ./app.json)
 readonly S3_SQL_PATH=s3://bistrosolutions/databases
 readonly S3_SQL_FILE=bistrosolutions.sql
 readonly S3_SQL_FILE_DOWNLOAD_TO=/tmp/${S3_SQL_FILE}
+# The `DEFINER` in the export SQL, the user who created procedures
+readonly EXPORT_DEFINER=terminal
 
 readonly BISTROSOL_DB_NAME=bistrosolutions
 readonly BISTROSOL_DB_DATABASE=${BISTROSOL_DB_NAME}_${APP_BRANCH}
-readonly BISTROSOL_DB_USER=replication
+
+# SSL
+readonly BISTROSOL_DB_USER=${BISTROSOL_DB_NAME}_replication_${APP_BRANCH}
 readonly BISTROSOL_DB_PASSWORD=$(get_password)
+
+# Non-SSL
+readonly BISTROSOL_DB_USER2=${BISTROSOL_DB_NAME}_web_${APP_BRANCH}
+readonly BISTROSOL_DB_PASSWORD2=$(get_password)
 
 readonly ROOT_DB_HOST=$(jq -r ".root.endpoint" ./db.json)
 readonly ROOT_DB_USER=$(jq -r ".root.user" ./db.json)
@@ -24,19 +45,37 @@ readonly ROOT_DB_PORT=$(jq -r ".root.port" ./db.json)
 
 readonly DB_JSON_FILE=${BISTROSOL_DB_NAME}-${APP_BRANCH}.json
 
+# Drop database
 no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
   -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
   -e "DROP DATABASE IF EXISTS $BISTROSOL_DB_DATABASE;"
+
+# Create database
 no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
   -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
   -e "CREATE DATABASE $BISTROSOL_DB_DATABASE;"
+
+# Create user 1
 no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
   -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
   -e "CREATE USER IF NOT EXISTS '$BISTROSOL_DB_USER'@'%' IDENTIFIED BY '$BISTROSOL_DB_PASSWORD' REQUIRE SSL;"
-# If user password changed
+
+# Update user 1 password
 no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
   -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
   -e "ALTER USER '$BISTROSOL_DB_USER'@'%' IDENTIFIED BY '$BISTROSOL_DB_PASSWORD' REQUIRE SSL;"
+
+# Create user 2
+no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
+  -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
+  -e "CREATE USER IF NOT EXISTS '$BISTROSOL_DB_USER2'@'%' IDENTIFIED BY '$BISTROSOL_DB_PASSWORD2';"
+
+# Update user 2 password
+no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
+  -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
+  -e "ALTER USER '$BISTROSOL_DB_USER2'@'%' IDENTIFIED BY '$BISTROSOL_DB_PASSWORD2';"
+
+# Grant privileges
 no_pw_warning mysql -h$ROOT_DB_HOST -u$ROOT_DB_USER \
   -p$ROOT_DB_PASSWORD -P$ROOT_DB_PORT \
   -e "GRANT ALL PRIVILEGES ON $BISTROSOL_DB_DATABASE.* TO '$BISTROSOL_DB_USER'@'%';"
@@ -51,11 +90,16 @@ echo Created database $BISTROSOL_DB_DATABASE
 echo Downloading database
 aws s3 cp --profile $AWS_PROFILE ${S3_SQL_PATH}/${S3_SQL_FILE} $S3_SQL_FILE_DOWNLOAD_TO
 
-# Replace database name in SQL
+# Replace database name in SQL (user `LC_ALL=C` to avoid error: illegal byte sequence)
 LC_ALL=C sed -i '' -e "s/\`${BISTROSOL_DB_NAME}\`/\`${BISTROSOL_DB_NAME}_${APP_BRANCH}\`/g" $S3_SQL_FILE_DOWNLOAD_TO
 
-echo Loading database
-#no_pw_warning
+# Remove DEFINER because in RDS you don't have SUPER privileges
+# Error: You do not have the SUPER privilege and binary logging is enabled (you *might* want to use the less safe log_bin_trust_function_creators variable)
+# https://www.percona.com/blog/2014/07/02/using-mysql-triggers-and-views-in-amazon-rds/#comment-10968243
+LC_ALL=C sed -i '' -e "s#/\*!50017 DEFINER=\`${EXPORT_DEFINER}\`@\`%\`\*/##g" $S3_SQL_FILE_DOWNLOAD_TO
+LC_ALL=C sed -i '' -e "s#DEFINER=\`${EXPORT_DEFINER}\`@\`%\` ##g" $S3_SQL_FILE_DOWNLOAD_TO 
+
+echo Loading database. This is going to take a while, please wait.
 mysql -h$ROOT_DB_HOST -u$BISTROSOL_DB_USER \
   -p$BISTROSOL_DB_PASSWORD -P$ROOT_DB_PORT < $S3_SQL_FILE_DOWNLOAD_TO
 
@@ -66,6 +110,13 @@ echo "{
     \"database\": \"$BISTROSOL_DB_DATABASE\",
     \"user\": \"$BISTROSOL_DB_USER\",
     \"password\": \"$BISTROSOL_DB_PASSWORD\",
+    \"port\": \"$ROOT_DB_PORT\"
+  },
+  \"${BISTROSOL_DB_USER2}\": {
+    \"endpoint\": \"$ROOT_DB_HOST\",
+    \"database\": \"$BISTROSOL_DB_DATABASE\",
+    \"user\": \"$BISTROSOL_DB_USER2\",
+    \"password\": \"$BISTROSOL_DB_PASSWORD2\",
     \"port\": \"$ROOT_DB_PORT\"
   }
 }" > $DB_JSON_FILE
