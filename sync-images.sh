f4bb321c42a9e72c779af4d85291311066c95a2f
Alan Zhao <alanzhaonys@gmail.com>
2018-06-28 02:07:28 -0400

    fix sync-image.sh

--- sync-images.sh
+++ sync-images.sh
@@ -15,8 +15,11 @@ readonly DEST_BRANCH=$2
 readonly UPLOAD_S3_BUCKET=$(jq -r ".aws.wordpressUploadS3Bucket" ./app.json)
 readonly UPLOAD_S3_BUCKET_INNER_PATH=wp-content/uploads
 
-readonly ORIGIN_PATH=${UPLOAD_S3_BUCKET}/${APP_NAME}/${ORIGIN_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
-readonly DEST_PATH=${UPLOAD_S3_BUCKET}/${APP_NAME}/${DEST_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
+readonly ORIGIN_PATH_FOLDER=${APP_NAME}/${ORIGIN_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
+readonly DEST_PATH_FOLDER=${APP_NAME}/${DEST_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
+
+readonly ORIGIN_PATH=${UPLOAD_S3_BUCKET}/${ORIGIN_PATH_FOLDER}
+readonly DEST_PATH=${UPLOAD_S3_BUCKET}/${DEST_PATH_FOLDER}
 
 readonly ORIGIN_URL=https://s3.amazonaws.com/${ORIGIN_PATH}
 readonly DEST_URL=https://s3.amazonaws.com/${DEST_PATH}
@@ -41,3 +44,7 @@ echo Replacing image URLs in database
 no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${ORIGIN_URL}', '${DEST_URL}');"
 
 no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${ORIGIN_URL}', '${DEST_URL}');"
+
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${ORIGIN_PATH_FOLDER}', '${DEST_PATH_FOLDER}');"
+
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${ORIGIN_PATH_FOLDER}', '${DEST_PATH_FOLDER}');"
