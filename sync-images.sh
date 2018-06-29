e87fe462cae5c6a8cde2fd5580e842a0170ba449
Alan Zhao <alanzhaonys@gmail.com>
2018-06-29 13:21:10 -0400

    reset-wordpress.sh and sync-images.sh fixes

--- sync-images.sh
+++ sync-images.sh
@@ -15,14 +15,15 @@ readonly DEST_BRANCH=$2
 readonly UPLOAD_S3_BUCKET=$(jq -r ".aws.wordpressUploadS3Bucket" ./app.json)
 readonly UPLOAD_S3_BUCKET_INNER_PATH=wp-content/uploads
 
-readonly ORIGIN_PATH_FOLDER=${APP_NAME}/${ORIGIN_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
-readonly DEST_PATH_FOLDER=${APP_NAME}/${DEST_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
+readonly ORIGIN_PATH=${APP_NAME}/${ORIGIN_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
+readonly DEST_PATH=${APP_NAME}/${DEST_BRANCH}/${UPLOAD_S3_BUCKET_INNER_PATH}
 
-readonly ORIGIN_PATH=${UPLOAD_S3_BUCKET}/${ORIGIN_PATH_FOLDER}
-readonly DEST_PATH=${UPLOAD_S3_BUCKET}/${DEST_PATH_FOLDER}
+readonly ORIGIN_BUCKET=${UPLOAD_S3_BUCKET}/${ORIGIN_PATH}
+readonly DEST_BUCKET=${UPLOAD_S3_BUCKET}/${DEST_PATH}
 
-readonly ORIGIN_URL=https://s3.amazonaws.com/${ORIGIN_PATH}
-readonly DEST_URL=https://s3.amazonaws.com/${DEST_PATH}
+readonly DEV_ORIGIN_PATH=${APP_NAME}/dev/${UPLOAD_S3_BUCKET_INNER_PATH}
+readonly QA_ORIGIN_PATH=${APP_NAME}/qa/${UPLOAD_S3_BUCKET_INNER_PATH}
+readonly MASTER_ORIGIN_PATH=${APP_NAME}/master/${UPLOAD_S3_BUCKET_INNER_PATH}
 
 readonly DEST_DB_HOST=$(jq -r ".${DEST_BRANCH}.endpoint" $DB_CONFIG_FILE)
 readonly DEST_DB_DATABASE=$(jq -r ".${DEST_BRANCH}.database" $DB_CONFIG_FILE)
@@ -33,18 +34,19 @@ readonly DEST_DB_PORT=$(jq -r ".${DEST_BRANCH}.port" $DB_CONFIG_FILE)
 # Sync images between buckets
 echo Syncing images
 
-echo Origin path: $ORIGIN_PATH
-echo Destination path: $DEST_PATH
+echo Origin path: $ORIGIN_BUCKET
+echo Destination path: $DEST_BUCKET
 
-aws s3 sync --profile $AWS_PROFILE s3://${ORIGIN_PATH} s3://${DEST_PATH}
+aws s3 sync --profile $AWS_PROFILE s3://${ORIGIN_BUCKET} s3://${DEST_BUCKET}
 
 # Replace image URLs in database
 echo Replacing image URLs in database
 
-no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${ORIGIN_URL}', '${DEST_URL}');"
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${DEV_ORIGIN_PATH}', '${DEST_PATH}');"
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${DEV_ORIGIN_PATH}', '${DEST_PATH}');"
 
-no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${ORIGIN_URL}', '${DEST_URL}');"
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${QA_ORIGIN_PATH}', '${DEST_PATH}');"
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${QA_ORIGIN_PATH}', '${DEST_PATH}');"
 
-no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${ORIGIN_PATH_FOLDER}', '${DEST_PATH_FOLDER}');"
-
-no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${ORIGIN_PATH_FOLDER}', '${DEST_PATH_FOLDER}');"
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_posts SET post_content = replace(post_content, '${MASTER_ORIGIN_PATH}', '${DEST_PATH}');"
+no_pw_warning mysql -h$DEST_DB_HOST -u$DEST_DB_USER -p$DEST_DB_PASSWORD -e "UPDATE ${DEST_DB_DATABASE}.wp_postmeta SET meta_value = replace(meta_value, '${MASTER_ORIGIN_PATH}', '${DEST_PATH}');"
