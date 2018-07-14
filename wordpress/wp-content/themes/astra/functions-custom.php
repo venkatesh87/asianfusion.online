ccf73f7ca5a505dabacfc392a665b2dbbdefeeb5
Alan Zhao <alanzhaonys@gmail.com>
2018-07-14 03:31:31 -0400

    further clean up dashboard

--- wordpress/wp-content/themes/astra/functions-custom.php
+++ wordpress/wp-content/themes/astra/functions-custom.php
@@ -118,6 +118,31 @@ function add_custom_dashboard_widget() {
 add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
 
 //
+// Remove admin bar logo
+//
+function remove_wp_logo( $wp_admin_bar ) {
+	$wp_admin_bar->remove_node( 'wp-logo' );
+}
+add_action( 'admin_bar_menu', 'remove_wp_logo', 999 );
+
+//
+// Remove footer link
+//
+function remove_footer_link ()
+{
+    echo '<span id="footer-thankyou"></span>';
+}
+add_filter('admin_footer_text', 'remove_footer_link' );
+
+//
+// Remove version from dashboard
+//
+function remove_footer_version() {
+    remove_filter( 'update_footer', 'core_update_footer' );
+}
+add_action( 'admin_menu', 'remove_footer_version' );
+
+//
 // Nice search - Redirects search results from /?s=query to /search/query/
 // https://github.com/roots/soil/blob/master/modules/nice-search.php
 //
