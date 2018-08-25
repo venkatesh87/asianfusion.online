784f7b422149575c9649ab04ce360fd9f762bfd4
Alan Zhao <alanzhaonys@gmail.com>
2018-08-25 19:05:21 -0400

    refactor

--- wordpress/wp-content/themes/allsol/woocommerce/emails/email-addresses.php
+++ wordpress/wp-content/themes/allsol/woocommerce/emails/email-addresses.php
@@ -31,7 +31,6 @@ $text_align = is_rtl() ? 'right' : 'left';
 				<?php echo ( $address = $order->get_formatted_billing_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
 				<?php if ( $order->get_billing_phone() ) : ?>
 					<br/><?php echo esc_html( $order->get_billing_phone() ); ?>
-        <?php endif; ?>
 
         <?php
           // Customization block: add billing phone extension
@@ -40,6 +39,8 @@ $text_align = is_rtl() ? 'right' : 'left';
             echo 'ext. ' . esc_html( $billing_phone_ext );
           }
         ?>
+
+        <?php endif; ?>
 
 				<?php if ( $order->get_billing_email() ) : ?>
 					<p><?php echo esc_html( $order->get_billing_email() ); ?></p>
