57016a0e05788d369b0dd7cbc17bc5ca4674b065
Alan Zhao <alanzhaonys@gmail.com>
2018-09-07 10:04:41 -0400

    db scripts

--- sync-db-up.sh
+++ sync-db-up.sh
@@ -10,8 +10,14 @@ fi
 readonly DB_BACKUP_S3_BUCKET=$(jq -r ".aws.dbBackupS3Bucket" ./app.json)
 readonly DATABASE=$(jq -r ".${BRANCH}.database" ./db.json)
 readonly DATE=`date '+%Y-%m-%d-%H-%M-%S'`
-readonly SQL_FILE=${DATABASE}.sql
 readonly S3_SQL_FILE=${DATABASE}_${DATE}.sql
+
+SQL_FILE=${DATABASE}.sql
+
+if [ "$BRANCH" == "local" ]; then
+  DB_NAME=$(jq -r ".rds.instanceName" ./app.json)
+  SQL_FILE=${DB_NAME}_local.sql
+fi
 
 sh ./dump-db.sh $BRANCH
 
