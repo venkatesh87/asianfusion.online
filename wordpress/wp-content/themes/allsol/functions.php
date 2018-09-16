1686f0702864320fbb98459daa8cfc3c4f7a42be
Alan Zhao <alanzhaonys@gmail.com>
2018-09-16 00:53:28 -0400

    fix client-script path

--- wordpress/wp-content/themes/allsol/functions.php
+++ wordpress/wp-content/themes/allsol/functions.php
@@ -22,7 +22,7 @@ function enqueue_styles() {
       wp_get_theme()->get('Version')
     );
 
-  wp_enqueue_script( $client_script, get_template_directory_uri() . '/client-script.js', array('jquery'), '1.0.0', true );
+  wp_enqueue_script( $client_script, get_stylesheet_directory_uri() . '/client-script.js', array('jquery'), '1.0.0', true );
 
 }
 
